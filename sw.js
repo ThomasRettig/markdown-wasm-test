d7f48e3274c8b3738e597cc6f9fb2b8169066ecd
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-04-18 16:33:55 +0800

    Format code

--- sw.js
+++ sw.js
@@ -1,41 +1,38 @@
-var CACHE = 'cache-v1';
+var CACHE = "cache-v1";
 
 var urls = [
-  '/',
-  'styles.css',
-  'clipboard.min.js',
-  'favicon.svg',
-  'manifest.webmanifest',
-  'markdown.js',
-  'highlight.css',
-  'highlight.js',
-  'sw.js',
-  'markdown.wasm',
-  'https://rsms.me/res/fonts/iaw.css',
-  'https://rsms.me/inter/inter.css',
-  'https://rsms.me/inter/font-files/Inter-roman.var.woff2?v=3.19',
-  'https://rsms.me/res/fonts/iaw/iaw-mono-vf.woff2',
-  'icon-192x192.png' // default PWA icon fetched by Chrome desktop
+  "/",
+  "styles.css",
+  "clipboard.min.js",
+  "favicon.svg",
+  "manifest.webmanifest",
+  "markdown.js",
+  "highlight.css",
+  "highlight.js",
+  "sw.js",
+  "markdown.wasm",
+  "https://rsms.me/res/fonts/iaw.css",
+  "https://rsms.me/inter/inter.css",
+  "https://rsms.me/inter/font-files/Inter-roman.var.woff2?v=3.19",
+  "https://rsms.me/res/fonts/iaw/iaw-mono-vf.woff2",
+  "icon-192x192.png", // default PWA icon fetched by Chrome desktop
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE)
-      .then(function(cache) {
-        return cache.addAll(urls);
-      })
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll(urls);
+    })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
       }
-    )
+      return fetch(event.request);
+    })
   );
 });
