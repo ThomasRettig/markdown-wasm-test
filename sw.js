19eea3af0886fafca4a7cd2a301c388599f7c485
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-04-22 21:42:59 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ var urls = [
   "https://rsms.me/inter/font-files/Inter-roman.var.woff2?v=3.19",
   "https://rsms.me/res/fonts/iaw/iaw-mono-vf.woff2",
   "icon-192x192.png", // default PWA icon fetched by Chrome desktop
-  "apple-touch-icon-512x512.png",
+  "apple-touch-icon-180x180.png",
 ];
 
 self.addEventListener("install", function (event) {
